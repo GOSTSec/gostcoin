5e643aeac9274e969f4070798cb744ac4e3f416a
orignal <i2porignal@yandex.ru>
2018-02-06 14:50:11 -0500

    use EVP_CIPHER_CTX from openssl 1.1

--- src/Crypto.h
+++ src/Crypto.h
@@ -3,6 +3,7 @@
 
 #include <openssl/bn.h>
 #include <openssl/ecdsa.h>
+#include <openssl/evp.h>
 
 #include <openssl/opensslv.h>
 #if (OPENSSL_VERSION_NUMBER < 0x010100000) || defined(LIBRESSL_VERSION_NUMBER) // 1.1.0 or LibreSSL
@@ -16,6 +17,12 @@ inline int ECDSA_SIG_set0(ECDSA_SIG *sig, BIGNUM *r, BIGNUM *s)
 	}
 inline void ECDSA_SIG_get0(const ECDSA_SIG *sig, const BIGNUM **pr, const BIGNUM **ps)
 	{ *pr = sig->r; *ps = sig->s; }
+
+// EVP
+inline int EVP_CIPHER_CTX_reset(EVP_CIPHER_CTX *ctx)
+	{
+		return EVP_CIPHER_CTX_cleanup (ctx);
+	}
 
 // ssl
 #define TLS_method TLSv1_method
