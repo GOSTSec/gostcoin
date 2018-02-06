20d9569fd26e2ca2b6ef52cfc6ac7585958b2744
orignal <i2porignal@yandex.ru>
2018-02-06 13:52:17 -0500

    CBigNum for openssl 1.1

--- src/base58.h
+++ src/base58.h
@@ -50,7 +50,7 @@ inline std::string EncodeBase58(const unsigned char* pbegin, const unsigned char
     CBigNum rem;
     while (bn > bn0)
     {
-        if (!BN_div(&dv, &rem, &bn, &bn58, pctx))
+        if (!BN_div(dv.GetBN (), rem.GetBN (), bn.GetBN (), bn58.GetBN (), pctx))
             throw bignum_error("EncodeBase58 : BN_div failed");
         bn = dv;
         unsigned int c = rem.getulong();
@@ -97,7 +97,7 @@ inline bool DecodeBase58(const char* psz, std::vector<unsigned char>& vchRet)
             break;
         }
         bnChar.setulong(p1 - pszBase58);
-        if (!BN_mul(&bn, &bn, &bn58, pctx))
+        if (!BN_mul(bn.GetBN (), bn.GetBN (), bn58.GetBN (), pctx))
             throw bignum_error("DecodeBase58 : BN_mul failed");
         bn += bnChar;
     }
