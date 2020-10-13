f9a159051dd329fbbe8acbdfd3342045607544c1
orignal <i2porignal@yandex.ru>
2020-10-13 15:38:08 -0400

    fixed build with boost >= 1.70

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -590,7 +590,11 @@ public:
     }
     bool connect(const std::string& server, const std::string& port)
     {
+#if (BOOST_VERSION >= 107000) // boost >= 1.70
+		ip::tcp::resolver resolver(stream.get_executor().context());
+#else
         ip::tcp::resolver resolver(stream.get_io_service());
+#endif
         ip::tcp::resolver::query query(server.c_str(), port.c_str());
         ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
         ip::tcp::resolver::iterator end;
@@ -677,7 +681,11 @@ static void RPCListen(boost::shared_ptr< basic_socket_acceptor<Protocol, SocketA
                    const bool fUseSSL)
 {
     // Accept connection
+#if (BOOST_VERSION >= 107000) // boost >= 1.70
+	AcceptedConnectionImpl<Protocol>* conn = new AcceptedConnectionImpl<Protocol>(acceptor->get_executor().context(), context, fUseSSL);
+#else
     AcceptedConnectionImpl<Protocol>* conn = new AcceptedConnectionImpl<Protocol>(acceptor->get_io_service(), context, fUseSSL);
+#endif
 
     acceptor->async_accept(
             conn->sslStream.lowest_layer(),
